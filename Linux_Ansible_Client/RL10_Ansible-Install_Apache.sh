c0fc36fab06631f94d577c15bbcf9b2340b36424
Dave Frankel <dfrankel33@gmail.com>
2019-02-14 09:55:24 -0600

    commit it

--- Linux_Ansible_Client/RL10_Ansible-Install_Apache.sh
+++ Linux_Ansible_Client/RL10_Ansible-Install_Apache.sh
@@ -2,12 +2,18 @@
 # ---
 # RightScript Name: RL10 Ansible - Install Apache
 # Description: Check whether a RightLink upgrade is available and perform the upgrade.
-# Inputs: {}
-# Attachments:
-# - apache.yml
+# Inputs:
+#   PLAYBOOK_URL:
+#     Category: Ansible
+#     Description: URL of an Ansible Playbook YML file
+#     Input Type: single
+#     Required: true
+#     Advanced: false
+#     Default: text:https://raw.githubusercontent.com/dfrankel33/rs-st/master/Linux_Ansible_Client/attachments/apache.yml
+# Attachments: []
 # ...
 
 
-attachments=${RS_ATTACH_DIR:-attachments}
+curl $PLAYBOOK_URL --output playbook.yml
 
-sudo ansible-playbook ${attachments}/apache.yml
+sudo ansible-playbook playbook.yml
